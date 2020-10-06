fb51fd3c6b6ea293acff2a71741e698be48d67fe
Federico <federico.izzop@gmail.com>
2020-10-06 11:56:13 +0200

    first try

--- sketch.js
+++ sketch.js
@@ -1,12 +1,37 @@
-function preload(){
-  // put preload code here
-}
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
-  // put drawing code here
+  background(0,20);
+
+  for(var x=10; x<=windowWidth; x+=100) {
+    for(var y=20; y<=windowHeight; y+=100) {
+
+      push();
+      translate(x,y);
+      rotate(random(frameCount/20));
+      // fill('aqua');
+      noFill();
+      strokeWeight(1);
+      stroke("aqua")
+      rect(10,20,20,20);
+      pop();
+    }
+  }
+
+  for(var x=30; x<=windowWidth; x+=100) {
+    for(var y=50; y<=windowHeight; y+=100) {
+
+      push();
+      translate(x,y);
+      rotate(-frameCount/20);
+      noFill();
+      strokeWeight(1);
+      stroke("lime")
+      circle(10,20,40);
+      pop();
+    }
+  }
 }
